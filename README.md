d51638267be286f2726bd17a3fafc0098f94ea13
Riddhiman Ghatak <riddhimanghatak@gmail.com>
2024-09-03 17:04:09 +0530

    minor changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Data_analysis_MySQL
 
 ## Overview
-fetched data from kaggle . Did some some basic data cleaning. Then did data analysis using MySQL.
+The layoff data is taken from kaggle. Did basic data cleaning and then did EDA using MySQL
--- data_analysis.sql
+++ data_analysis.sql
@@ -1,80 +1,150 @@
---find top 10 highest reveue generating products 
-select top 10 product_id,sum(sale_price) as sales
-from df_orders
-group by product_id
-order by sales desc
+-- EDA
 
+-- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers
 
+-- normally when you start the EDA process you have some idea of what you're looking for
 
+-- with this info we are just going to look around and see what we find!
 
---find top 5 highest selling products in each region
-with cte as (
-select region,product_id,sum(sale_price) as sales
-from df_orders
-group by region,product_id)
-select * from (
-select *
-, row_number() over(partition by region order by sales desc) as rn
-from cte) A
-where rn<=5
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
 
+-- EASIER QUERIES
 
-
---find month over month growth comparison for 2022 and 2023 sales eg : jan 2022 vs jan 2023
-with cte as (
-select year(order_date) as order_year,month(order_date) as order_month,
-sum(sale_price) as sales
-from df_orders
-group by year(order_date),month(order_date)
---order by year(order_date),month(order_date)
-	)
-select order_month
-, sum(case when order_year=2022 then sales else 0 end) as sales_2022
-, sum(case when order_year=2023 then sales else 0 end) as sales_2023
-from cte 
-group by order_month
-order by order_month
+SELECT MAX(total_laid_off)
+FROM world_layoffs.layoffs_staging2;
 
 
 
 
 
---for each category which month had highest sales 
-with cte as (
-select category,format(order_date,'yyyyMM') as order_year_month
-, sum(sale_price) as sales 
-from df_orders
-group by category,format(order_date,'yyyyMM')
---order by category,format(order_date,'yyyyMM')
+
+-- Looking at Percentage to see how big these layoffs were
+SELECT MAX(percentage_laid_off),  MIN(percentage_laid_off)
+FROM world_layoffs.layoffs_staging2
+WHERE  percentage_laid_off IS NOT NULL;
+
+-- Which companies had 1 which is basically 100 percent of they company laid off
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE  percentage_laid_off = 1;
+-- these are mostly startups it looks like who all went out of business during this time
+
+-- if we order by funcs_raised_millions we can see how big some of these companies were
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE  percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+-- BritishVolt looks like an EV company, Quibi! I recognize that company - wow raised like 2 billion dollars and went under - ouch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SOMEWHAT TOUGHER AND MOSTLY USING GROUP BY--------------------------------------------------------------------------------------------------
+
+-- Companies with the biggest single Layoff
+
+SELECT company, total_laid_off
+FROM world_layoffs.layoffs_staging
+ORDER BY 2 DESC
+LIMIT 5;
+-- now that's just on a single day
+
+-- Companies with the most Total Layoffs
+SELECT company, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+LIMIT 10;
+
+
+
+-- by location
+SELECT location, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY location
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- this it total in the past 3 years or in the dataset
+
+SELECT country, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT YEAR(date), SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY YEAR(date)
+ORDER BY 1 ASC;
+
+
+SELECT industry, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+
+SELECT stage, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+
+
+
+
+
+-- TOUGHER QUERIES------------------------------------------------------------------------------------------------------------------------------------
+
+-- Earlier we looked at Companies with the most Layoffs. Now let's look at that per year. It's a little more difficult.
+-- I want to look at 
+
+WITH Company_Year AS 
+(
+  SELECT company, YEAR(date) AS years, SUM(total_laid_off) AS total_laid_off
+  FROM layoffs_staging2
+  GROUP BY company, YEAR(date)
 )
-select * from (
-select *,
-row_number() over(partition by category order by sales desc) as rn
-from cte
-) a
-where rn=1
-
-
-
-
-
-
---which sub category had highest growth by profit in 2023 compare to 2022
-with cte as (
-select sub_category,year(order_date) as order_year,
-sum(sale_price) as sales
-from df_orders
-group by sub_category,year(order_date)
---order by year(order_date),month(order_date)
-	)
-, cte2 as (
-select sub_category
-, sum(case when order_year=2022 then sales else 0 end) as sales_2022
-, sum(case when order_year=2023 then sales else 0 end) as sales_2023
-from cte 
-group by sub_category
+, Company_Year_Rank AS (
+  SELECT company, years, total_laid_off, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
+  FROM Company_Year
 )
-select top 1 *
-,(sales_2023-sales_2022)
-from  cte2
-order by (sales_2023-sales_2022) desc
+SELECT company, years, total_laid_off, ranking
+FROM Company_Year_Rank
+WHERE ranking <= 3
+AND years IS NOT NULL
+ORDER BY years ASC, total_laid_off DESC;
+
+
+
+
+-- Rolling Total of Layoffs Per Month
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC;
+
+-- now use it in a CTE so we can query off of it
+WITH DATE_CTE AS 
+(
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC
+)
+SELECT dates, SUM(total_laid_off) OVER (ORDER BY dates ASC) as rolling_total_layoffs
+FROM DATE_CTE
+ORDER BY dates ASC;
